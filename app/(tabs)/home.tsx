0c3b3633cc62ed08511221bb0a38586c0c9e1a6e
Oluwaseun241 <tanimolaoluwaseun70@gmail.com>
2025-03-14 15:20:24 +0100

    wip: home

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,10 +1,32 @@
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, Text, View } from "react-native";
+import Logo from "@/assets/images/login.svg";
+import Trend from "@/assets/icons/trend.svg";
+import Heart from "@/assets/icons/heart.svg";
+import Notification from "@/assets/icons/notification.svg";
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
-      <Text>Home Screen</Text>
+    <SafeAreaView className="flex-1 bg-white">
+      {/* Header */}
+      <View className="px-4 flex-row justify-between">
+        <View className="flex-row items-center gap-2">
+          <Logo width={40} />
+          <Text className="font-Neue text-sm">Samuel Adedoyin</Text>
+        </View>
+        <View className="flex-row items-center gap-4">
+          <Trend />
+          <Heart />
+          <Notification />
+        </View>
+      </View>
+      <View className="px-4">
+        <Text>Accounts</Text>
+        <Text>Savings</Text>
+        <Text>Cashback</Text>
+        <Text>Cards</Text>
+      </View>
+      {/* Account Card */}
     </SafeAreaView>
   );
-} 
+}
